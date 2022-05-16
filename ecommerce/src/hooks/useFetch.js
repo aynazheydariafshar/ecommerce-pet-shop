cd3bb48931038940ccae17654edfa28b435fe802
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-16 20:04:31 +0430

    feat : set usestate for data & error & loading

--- ecommerce/src/hooks/useFetch.js
+++ ecommerce/src/hooks/useFetch.js
@@ -1,5 +1,11 @@
-const useFetch = () => {
+import { useState } from "react"
 
+const useFetch = () => {
+    const [data , setData] = useState([]);
+    const [loading , setLoading] = useState(true);
+    const [error , setError] = useState(null);
+
+    
 }
 
 export default useFetch
