0332d5b396a019ca8507c299fe498522d53b568d
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-16 11:37:25 +0430

    feat : add footer with mui

--- ecommerce/src/Layout/structerLayout/Footer.js
+++ ecommerce/src/Layout/structerLayout/Footer.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import styled from '@emotion/styled';
+
+const FooterContainer = styled('div')({
+    color: 'white',
+    backgroundColor: 'black',
+    paddingTop: 10,
+    height: 30,
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+});
 
 const Footer = () => {
-    return <div>Footer</div>;
-}
+    return <Box sx={{ flexGrow: 1 }}>
+        <FooterContainer>
+            <Typography
+                    noWrap
+                    component="div"
+                    sx={{textAlign : 'center'}}
+                >
+                فروشگاه ما را در شبکه‌های اجتماعی دنبال کنید     
+            </Typography>
+            <Box>
+                <IconButton>
 
+                </IconButton>
+            </Box>
+        </FooterContainer>    
+  </Box>;
+}
 
 export default Footer;
