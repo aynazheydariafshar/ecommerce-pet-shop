6233feab31ad99d3935b34151e5929e9bbc6f6c6
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-16 14:46:49 +0430

    feat : add style and responsive page

--- ecommerce/src/Pages/Login.js
+++ ecommerce/src/Pages/Login.js
@@ -1,7 +1,9 @@
-import { Grid } from '@mui/material';
+import { Button, Grid, InputAdornment, TextField } from '@mui/material';
 import React from 'react';
 import LoginImage from 'assets/images/24237-Cat-Dog-CorgiCat-Dog-HD-Wallpaper.jpg'
 import Logo from 'assets/images/logo.png';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import KeyRoundedIcon from '@mui/icons-material/KeyRounded';
 
 const Login = () => {
     return <div>
@@ -9,11 +11,47 @@ const Login = () => {
             <Grid item xs={12} sm={6}>
                 <img alt='animal' src={LoginImage} width='100%' height='100%' style={{objectFit : 'cover'}}/>
             </Grid>
-            <Grid container xs={12} sm={6}>
-                <div>
+            <Grid 
+                container 
+                alignItems='center' 
+                direction='column'
+                justifyContent='space-around' 
+                xs={12} 
+                sm={6}
+            >
+                <div style={{display : 'flex' , flexDirection :'column' , maxWidth : 400 , minWidth : 300}}>
                     <Grid container justifyContent='center'>
                         <img src={Logo} alt="logo" width='50px' height='50px'/>
                     </Grid>
+                    <TextField  
+                        variant="standard" 
+                        type='text' 
+                        label='نام کاربری' 
+                        margin='normal'
+                        InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <PersonRoundedIcon />
+                              </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <TextField  
+                        variant="standard" 
+                        type='password' 
+                        label='رمز عبور' 
+                        margin='normal'
+                        InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <KeyRoundedIcon />
+                              </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <Button sx={{my : '20px'}} color='primary' variant='contained'>
+                        ورود
+                    </Button>
                 </div>
             </Grid>
         </Grid>
