82aa458c978a2d923476f96cc7dcecbe05ebb8c8
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-16 14:53:39 +0430

    feat : add icon for go to home page

--- ecommerce/src/Pages/Login.js
+++ ecommerce/src/Pages/Login.js
@@ -4,7 +4,8 @@ import LoginImage from 'assets/images/24237-Cat-Dog-CorgiCat-Dog-HD-Wallpaper.jp
 import Logo from 'assets/images/logo.png';
 import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
 import KeyRoundedIcon from '@mui/icons-material/KeyRounded';
-
+import DoorBackRoundedIcon from '@mui/icons-material/DoorBackRounded';
+import {Link} from "react-router-dom";
 const Login = () => {
     return <div>
         <Grid container sx = {{minHeight : '100vh' , overflowY : 'hidden'}}>
@@ -52,6 +53,9 @@ const Login = () => {
                     <Button sx={{my : '20px'}} color='primary' variant='contained'>
                         ورود
                     </Button>
+                    <Link to='/'>
+                        <DoorBackRoundedIcon color="action"/>
+                    </Link>
                 </div>
             </Grid>
         </Grid>
