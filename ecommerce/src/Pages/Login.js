8b384f84a816e1c7c2ee5f2686d7fb26b79809ad
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-16 14:03:33 +0430

    feat: add image and build structer grid

--- ecommerce/src/Pages/Login.js
+++ ecommerce/src/Pages/Login.js
@@ -1,7 +1,22 @@
+import { Grid } from '@mui/material';
 import React from 'react';
+import LoginImage from 'assets/images/24237-Cat-Dog-CorgiCat-Dog-HD-Wallpaper.jpg'
 
 const Login = () => {
-    return <div>Login</div>;
+    return <div>
+        <Grid container sx = {{minHeight : '100vh' , overflowY : 'hidden'}}>
+            <Grid item xs={12} sm={6}>
+                <img src={LoginImage} width='100%' height='100%' style={{objectFit : 'cover'}}/>
+            </Grid>
+            <Grid container xs={12} sm={6}>
+                <div>
+                    <Grid>
+
+                    </Grid>
+                </div>
+            </Grid>
+        </Grid>
+    </div>;
 }
 
 
