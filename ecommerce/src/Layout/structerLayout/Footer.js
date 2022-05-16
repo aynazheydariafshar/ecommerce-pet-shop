7e95f462042544a86ad7efef42876fcce606b390
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-16 11:51:03 +0430

    feat : add icon

--- ecommerce/src/Layout/structerLayout/Footer.js
+++ ecommerce/src/Layout/structerLayout/Footer.js
@@ -1,36 +1,35 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import styled from '@emotion/styled';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 
 const FooterContainer = styled('div')({
     color: 'white',
     backgroundColor: 'black',
     paddingTop: 10,
-    height: 30,
+    height: 100,
     position: 'absolute',
     bottom: 0,
     width: '100%',
 });
 
 const Footer = () => {
-    return <Box sx={{ flexGrow: 1 }}>
-        <FooterContainer>
+    return <FooterContainer>
             <Typography
                     noWrap
                     component="div"
-                    sx={{textAlign : 'center'}}
+                    sx={{textAlign : 'center' , fontSize : '15px'}}
                 >
-                فروشگاه ما را در شبکه‌های اجتماعی دنبال کنید     
+                فروشگاه ما را در شبکه‌های اجتماعی دنبال کنید  
+                <Box sx={{mt : '10px'}}>
+                    <InstagramIcon />
+                    <FacebookRoundedIcon />
+                </Box>
             </Typography>
-            <Box>
-                <IconButton>
-
-                </IconButton>
-            </Box>
         </FooterContainer>    
-  </Box>;
+
 }
 
 export default Footer;
