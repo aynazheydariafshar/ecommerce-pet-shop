88195bb78952a5a7449dc3dc5fd3859e08e52e49
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-16 20:08:22 +0430

    feat : add useeffect

--- ecommerce/src/hooks/useFetch.js
+++ ecommerce/src/hooks/useFetch.js
@@ -1,11 +1,25 @@
-import { useState } from "react"
+import { useState , useEffect} from "react"
 
 const useFetch = () => {
     const [data , setData] = useState([]);
     const [loading , setLoading] = useState(true);
     const [error , setError] = useState(null);
 
-    
+    useEffect(() => {
+       (async() => {
+           try {
+               setLoading(true);
+               const response = await fetch();
+               const resault = await response.json();
+               setData(resault);
+           } catch (error) {
+               setError(error);
+           } finally{
+                setLoading(false);
+           }
+       })
+    }, [])
+
 }
 
 export default useFetch
