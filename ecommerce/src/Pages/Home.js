663d4eda12c088b07cd2e23c234b26a781d8df3f
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-18 19:14:52 +0430

    feat : add card component file

--- ecommerce/src/Pages/Home.js
+++ ecommerce/src/Pages/Home.js
@@ -1,9 +1,12 @@
+import useFetch from 'hooks/useFetch';
 import CustomerLayout from 'layout/CustomerLayout';
 import React from 'react';
 
 const Home = () => {
+
+    const {data , loading , error} = useFetch('products');
+
     return <>
-        <h1>Home</h1>
     </>;
 };
 
