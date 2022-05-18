e518bbc00309ee635bbd6957a346002e258cca4c
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-18 23:36:18 +0430

    style : set navbarside

--- ecommerce/src/Layout/structerLayout/NavbarSide.js
+++ ecommerce/src/Layout/structerLayout/NavbarSide.js
@@ -4,32 +4,34 @@ import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import {FaCat , FaDog , FaKiwiBird} from 'react-icons/fa'
 
 const NavbarSide = () => {    
-    return <Container sx={{pt : '10px',overflowX : 'hidden' , textAlign : 'right'}}>
-        <IconButton>
-            <Typography fontSize='20px'>
-                خانه 
-            </Typography>
-            <HomeRoundedIcon />
-        </IconButton>
-        <IconButton>
-            <Typography  fontSize='20px'>
-                 محصولات گربه
-            </Typography>
-            <FaCat />
-        </IconButton> 
-        <IconButton>
-            <Typography  fontSize='20px'>
-                محصولات سگ ها 
-            </Typography>
-            <FaDog />
-        </IconButton> 
-        <IconButton>
-            <Typography  fontSize='20px'>
-                محصولات پرندگان 
-            </Typography>
-            <FaKiwiBird />
-        </IconButton> 
-    </Container>
+    return <div style={{overflowX : 'hidden',top : 0 , height:'100vh',position:'sticky' ,textAlign : 'right',boxShadow: 'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px' , zIndex : 1 , alignItems: 'center' }}>
+        <Container sx={{mt : '10px' , display :'flex' , flexDirection : 'column' , alignItems : 'flex-end'}}>
+            <IconButton className='menu'>
+                <Typography fontSize='17px' marginRight='8px'>
+                    خانه 
+                </Typography>
+                <HomeRoundedIcon />
+            </IconButton>
+            <IconButton className='menu'>
+                <Typography  fontSize='17px' marginRight='8px'>
+                    محصولات گربه
+                </Typography>
+                <FaCat fontSize='23px'/>
+            </IconButton> 
+            <IconButton className='menu'>
+                <Typography  fontSize='17px' marginRight='8px'>
+                    محصولات سگ ها 
+                </Typography>
+                <FaDog fontSize='23px'/>
+            </IconButton> 
+            <IconButton className='menu'>
+                <Typography  fontSize='17px' marginRight='8px'>
+                    محصولات پرندگان 
+                </Typography>
+                <FaKiwiBird fontSize='23px'/>
+            </IconButton> 
+        </Container>
+    </div>
 }
 
 
