6258336d153a596e585cc1de309efd6bae3c5f7f
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-18 19:20:56 +0430

    feat : add props on card components

--- ecommerce/src/Components/CardProduct.js
+++ ecommerce/src/Components/CardProduct.js
@@ -4,27 +4,33 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import PropTypes from 'prop-types';
 
-export default function CardProduct({}) {
+export default function CardProduct({imageSrc , titleCard , price}) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image={imageSrc}
           alt="green iguana"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {titleCard}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {price}
           </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
   );
+}
+
+ModalField.propTypes = {
+    imageSrc : PropTypes.string,
+    titleCard : PropTypes.string,
+    childeren : PropTypes.number,
 }
