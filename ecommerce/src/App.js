2814a46751b02341a144e1ded55274faa44613e2
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-19 10:58:12 +0430

    fix : when refresh logout

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,7 +1,20 @@
 import './styles/App.css';
 import RoutePages from 'routes/RoutePages';
+import { useEffect, useState } from 'react';
+import { getToken, removeUserSession, setUserSession } from 'utils/Common';
+import axios from 'axios';
 
 function App() {
+  useEffect(()=>{
+    const token = getToken();
+    if(!token){
+      return;
+    }
+    axios.get(`http://localhost:3002/auth/refresh-token?token=${token}`)
+      .then(response => {
+        setUserSession(response.data.token , response.data.user);
+      }).catch(er => removeUserSession())
+  }, [])
   return (
     <div className="App">
       <RoutePages />
