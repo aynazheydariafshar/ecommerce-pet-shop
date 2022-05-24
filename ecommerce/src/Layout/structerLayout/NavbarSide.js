cfedf3f6ba5869e8fc8362eb4a2a8d8bb9573f48
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-24 10:56:53 +0430

    feat : add sub group cat

--- ecommerce/src/Layout/structerLayout/NavbarSide.js
+++ ecommerce/src/Layout/structerLayout/NavbarSide.js
@@ -2,23 +2,47 @@ import { Container, IconButton, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import {FaCat , FaDog } from 'react-icons/fa';
-import {GiHummingbird} from 'react-icons/gi'
+import {GiHummingbird , GiOpenedFoodCan , GiHealthNormal} from 'react-icons/gi';
+import {RiScissors2Line} from 'react-icons/ri';
 
-const NavbarSide = () => {    
+
+const NavbarSide = () => { 
+    
+    const [showCat , setShowCat] = useState(false)
     return <div style={{overflowX : 'hidden',top : 0 , height:'100vh',position:'sticky' ,textAlign : 'right',boxShadow: 'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px' , zIndex : 1 , alignItems: 'center' , marginLeft:'13px' , backgroundColor : '#F8F9FD'}}>
-            <Container sx={{mt : '10px' , flexDirection : 'column' , alignItems : 'flex-end' , display : 'flex'}}>
+            <Container sx={{mt : '20px' , flexDirection : 'column' , alignItems : 'flex-end' , display : 'flex'}}>
                 <IconButton className='menu'>
                     <Typography fontSize='17px' marginRight='8px'>
                         خانه 
                     </Typography>
                     <HomeRoundedIcon />
                 </IconButton>
-                <IconButton className='menu'>
+                <IconButton className='menu' onClick={() => setShowCat(!showCat)}>
                     <Typography  fontSize='17px' marginRight='8px'>
                         محصولات گربه
                     </Typography>
                     <FaCat fontSize='23px'/>
                 </IconButton> 
+                {showCat && <>
+                <IconButton sx={{marginRight : '28px'}} className='menu'>
+                    <Typography  fontSize='15px' marginRight='8px'>
+                        غذای خشک گربه    
+                    </Typography>
+                    <GiOpenedFoodCan fontSize='18px'/>
+                </IconButton>
+                <IconButton sx={{marginRight : '28px'}} className='menu'>
+                    <Typography  fontSize='15px' marginRight='8px'>
+                        سلامتی گربه    
+                    </Typography>
+                    <GiHealthNormal fontSize='16px'/>
+                </IconButton>
+                <IconButton sx={{marginRight : '28px'}} className='menu'>
+                    <Typography  fontSize='15px' marginRight='8px'>
+                        آرایش و بهداشتی  گربه    
+                    </Typography>
+                    <RiScissors2Line fontSize='18px'/>
+                </IconButton>
+                </> }
                 <IconButton className='menu'>
                     <Typography  fontSize='17px' marginRight='8px'>
                         محصولات سگ ها 
