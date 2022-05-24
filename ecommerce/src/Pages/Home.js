940b978134dd740eccccd2bca4180f31e1140b6e
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-25 01:26:50 +0430

    feat : add category page

--- ecommerce/src/Pages/Home.js
+++ ecommerce/src/Pages/Home.js
@@ -1,32 +1,23 @@
-import { Box , Grid } from '@mui/material';
+import { Box } from '@mui/material';
 import CardProduct from 'components/CardProduct';
 import useFetch from 'hooks/useFetch';
 import CustomerLayout from 'layout/CustomerLayout';
-import NavbarSide from 'layout/structerLayout/NavbarSide';
 import React from 'react';
 
 const Home = () => {
 
     const {data , loading , error} = useFetch('products');
-
-    return <>
-        <Grid item md={9} sm={8} xs={7}>
-            <Box sx={{display : 'flex' , flexDirection : {md : 'row' , sm : 'column'} , justifyContent : 'space-around' , alignItems : 'center' , flexWrap : 'wrap' , py : '30px'}}>
-                {data.map(item => {
-                return <CardProduct 
-                        imageSrc={`http://localhost:3002/files/${item.image}`}
-                        titleCard={item.name}
-                        price = {item.price}
-                        brand = {item.brand}
-                    />
-                }) 
-                }
-            </Box>
-        </Grid>
-        <Grid item md={3} sm={4} xs={5}>
-            <NavbarSide />
-        </Grid>       
-    </>
+    return <Box sx={{display : 'flex' , flexDirection : {md : 'row' , sm : 'column'} , justifyContent : 'space-around' , alignItems : 'center' , flexWrap : 'wrap' , py : '30px'}}>
+    {data.map(item => {
+        return <CardProduct 
+            imageSrc={`http://localhost:3002/files/${item.image}`}
+            titleCard={item.name}
+            price = {item.price}
+            brand = {item.brand}
+        />
+    }) 
+    }
+    </Box> 
 };
 
 
