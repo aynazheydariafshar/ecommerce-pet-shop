d15023d9356e924cdf5e09eb46075666602bc210
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-25 11:44:32 +0430

    feat : add pagination number on page and responsive

--- ecommerce/src/Pages/Home.js
+++ ecommerce/src/Pages/Home.js
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Pagination, Stack } from '@mui/material';
 import CardProduct from 'components/CardProduct';
 import useFetch from 'hooks/useFetch';
 import CustomerLayout from 'layout/CustomerLayout';
@@ -7,17 +7,22 @@ import React from 'react';
 const Home = () => {
 
     const {data , loading , error} = useFetch('products');
-    return <Box sx={{display : 'flex' , flexDirection : {md : 'row' , sm : 'column'} , justifyContent : 'space-around' , alignItems : 'center' , flexWrap : 'wrap' , py : '30px'}}>
-    {data.map(item => {
-        return <CardProduct 
-            imageSrc={`http://localhost:3002/files/${item.image}`}
-            titleCard={item.name}
-            price = {item.price}
-            brand = {item.brand}
-        />
-    }) 
-    }
-    </Box> 
+    return <Box sx={{display : 'flex' , flexDirection : 'column' , alignItems : 'center'}}>
+        <Box sx={{display : 'flex' , flexDirection : {md : 'row' , sm : 'column'} , justifyContent : 'space-around' , alignItems : 'center' , flexWrap : 'wrap' , py : '30px'}}>
+        {data.map(item => {
+            return <CardProduct 
+                imageSrc={`http://localhost:3002/files/${item.image}`}
+                titleCard={item.name}
+                price = {item.price}
+                brand = {item.brand}
+            />
+        }) 
+        }
+        </Box> 
+        <Stack padding='40px'>
+            <Pagination count={10} color="secondary" />
+        </Stack>
+    </Box>
 };
 
 
