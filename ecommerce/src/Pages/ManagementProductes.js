c4d9cf7096cff848b0b5b40a16b1c757693cbd25
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-26 00:01:30 +0430

    feat : pagination products

--- ecommerce/src/Pages/ManagementProductes.js
+++ ecommerce/src/Pages/ManagementProductes.js
@@ -12,7 +12,9 @@ import useFetch from 'hooks/useFetch';
 import useCategory from 'hooks/useCategory';
 import {FaRegEdit , FaTrash} from 'react-icons/fa';
 import {MdOutlineAddCircleOutline} from 'react-icons/md';
-import { Button } from '@mui/material';
+import { Button, Pagination, Stack } from '@mui/material';
+import PaginationPage from 'components/PaginationPage';
+import { Box } from '@mui/system';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -44,61 +46,85 @@ function ManagementProductes() {
     //category
     const {category , loadingcategory , errorcategory} = useCategory();
 
+    // //pagination data
+    let [page, setPage] = React.useState(1);
+    const perPage = 6;
+  
+    const count = Math.ceil(data.length / perPage);
+    const product = PaginationPage(data, perPage);
+  
+    const handleChange = (e, p) => {
+      setPage(p);
+        product.jump(p);
+    };
+
   return (
       <>
         <Button color = "secondary"  variant="contained" sx={{padding : '20px' , margin : '30px'}} endIcon={<MdOutlineAddCircleOutline />}>
             افزودن کالا
         </Button>
-        <TableContainer dir='rtl' component={Paper} sx={{padding : '30px'}}>
-        <Table sx={{ minWidth: 700 }} aria-label="customized table">
-            <TableHead>
-            <TableRow>
-                <StyledTableCell>#</StyledTableCell>
-                <StyledTableCell align="right">نام کالا</StyledTableCell>
-                <StyledTableCell align="right">عکس</StyledTableCell>
-                <StyledTableCell align="right">گروه</StyledTableCell>
-                <StyledTableCell align="right">زیر گروه</StyledTableCell>
-                <StyledTableCell align="right">ویرایش</StyledTableCell>
-                <StyledTableCell align="right">حذف</StyledTableCell>
-            </TableRow>
-            </TableHead>
-            <TableBody>
-            {data.map((row) => (
-                <StyledTableRow key={row.name}>
-                <StyledTableCell component="th" scope="row">
-                    {row.id}
-                </StyledTableCell>
-                <StyledTableCell align="right" component="th" scope="row">
-                    {row.name}
-                </StyledTableCell>
-                <StyledTableCell align="right">
-                    <img src={`http://localhost:3002/files/${row.image}`} width='50px' height='50px'/>
-                </StyledTableCell>
-                    {category?.map(el => {
-                        if(row.category === el.id){
-                            return (
-                                <StyledTableCell align="right">{el.group}</StyledTableCell>
-                            )
-                        }
-                    })}
-                    {category?.map(el => {
-                        if(row.category === el.id){
-                            return (
-                                <StyledTableCell align="right">{el.subgroup}</StyledTableCell>
-                            )
-                        }
-                    })}
-                <StyledTableCell component="th" scope="row" className='icon-navbar'>
-                    <FaRegEdit />
-                </StyledTableCell>
-                <StyledTableCell component="th" scope="row" className='icon-trash'>
-                    <FaTrash />
-                </StyledTableCell>
-                </StyledTableRow>
-            ))}
-            </TableBody>
-        </Table>
-        </TableContainer>
+        <Box sx={{display : 'flex' , flexDirection : 'column' , alignItems : 'center'}}>
+            <TableContainer dir='rtl' component={Paper} sx={{padding : '30px'}}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                <TableRow>
+                    <StyledTableCell>#</StyledTableCell>
+                    <StyledTableCell align="right">نام کالا</StyledTableCell>
+                    <StyledTableCell align="right">عکس</StyledTableCell>
+                    <StyledTableCell align="right">گروه</StyledTableCell>
+                    <StyledTableCell align="right">زیر گروه</StyledTableCell>
+                    <StyledTableCell align="right">ویرایش</StyledTableCell>
+                    <StyledTableCell align="right">حذف</StyledTableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {product.currentData()?.map((row) => (
+                    <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
+                        {row.id}
+                    </StyledTableCell>
+                    <StyledTableCell align="right" component="th" scope="row">
+                        {row.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                        <img src={`http://localhost:3002/files/${row.image}`} width='50px' height='50px'/>
+                    </StyledTableCell>
+                        {category?.map(el => {
+                            if(row.category === el.id){
+                                return (
+                                    <StyledTableCell align="right">{el.group}</StyledTableCell>
+                                )
+                            }
+                        })}
+                        {category?.map(el => {
+                            if(row.category === el.id){
+                                return (
+                                    <StyledTableCell align="right">{el.subgroup}</StyledTableCell>
+                                )
+                            }
+                        })}
+                    <StyledTableCell component="th" scope="row" className='icon-navbar'>
+                        <FaRegEdit />
+                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row" className='icon-trash'>
+                        <FaTrash />
+                    </StyledTableCell>
+                    </StyledTableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+
+            <Stack padding='30px'>
+                <Pagination 
+                    size="large" 
+                    count={count} 
+                    color="secondary" 
+                    onChange={handleChange}
+                    page={page}
+                />
+            </Stack>
+        </Box>
       </>
   );
 }
