66437e1d41d4e6a5e3e7e275c1f3b91f3b47d30a
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-25 20:28:59 +0430

    feat : pagination cotegory

--- ecommerce/src/Pages/Category.js
+++ ecommerce/src/Pages/Category.js
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material";
+import { Box, Pagination, Stack } from "@mui/material";
+import PaginationPage from "components/PaginationPage";
 import useFetch from "hooks/useFetch";
 import CustomerLayout from "layout/CustomerLayout";
 import { useEffect, useState } from "react";
@@ -9,18 +10,30 @@ import CardProduct from "../components/CardProduct";
 const Category = () => {
     const dataParams = useParams();
     const {data , loading , error} = useFetch('products');
-    const [product , setproduct] = useState()
+    const [product , setproduct] = useState(data)
     const filterProduct = () => {
         return data.filter(item => item.category === +dataParams.idcategory);
     }
-    
+
+    let [page, setPage] = useState(1);
+    const perPage = 6;
+  
+    const count = Math.ceil(product.length / perPage);
+    const productPage = PaginationPage(product, perPage);
+  
+    const handleChange = (e, p) => {
+      setPage(p);
+      productPage.jump(p);
+    };
+
     useEffect(() => {
         setproduct(filterProduct())
     }, [product])
 
     return (
+    <Box sx={{display : 'flex' , flexDirection : 'column' , alignItems : 'center'}}>
         <Box sx={{display : 'flex' , flexDirection : {md : 'row' , sm : 'column'} , justifyContent : 'space-around' , alignItems : 'center' , flexWrap : 'wrap' , py : '30px'}}>
-        {product?.map(item => {
+        {productPage.currentData()?.map(item => {
            return <Link className='link' to={`/products/${item.id}`}>
             <CardProduct 
                     imageSrc={`http://localhost:3002/files/${item.image}`}
@@ -31,6 +44,16 @@ const Category = () => {
             </Link>
         }) 
         }
+        </Box>
+        <Stack padding='30px'>
+            <Pagination 
+                size="large" 
+                count={count} 
+                color="secondary" 
+                onChange={handleChange}
+                page={page}
+            />
+        </Stack>
     </Box>
     )
 
