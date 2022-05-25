92d30cf92cd426c786eea83cb25b5d76d5c2aa9d
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-25 14:53:57 +0430

    feat : add fuc to filter data

--- ecommerce/src/Pages/Product.js
+++ ecommerce/src/Pages/Product.js
@@ -1,9 +1,25 @@
+import useFetch from 'hooks/useFetch';
 import CustomerLayout from 'layout/CustomerLayout';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const Product = () => {
-    return <>
-        <h1>Product</h1>
+
+    const dataParams = useParams();
+    const {data , loading , error} = useFetch('products');
+    const [product , setProduct] = useState();
+
+    const filterData = () => {
+        return data.filter(item => item.id === +dataParams.id)
+    }
+
+    useEffect(() => {
+        setProduct(filterData);
+    }, [product])
+   
+   return <>
+        {product?.map(item =><h1>{item.name}</h1>)}
     </>;
 };
 
