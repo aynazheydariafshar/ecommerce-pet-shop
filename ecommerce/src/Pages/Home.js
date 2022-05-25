1d9de07762a5f6bf0b561de2d3a286e20cc68ba8
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-05-25 12:12:59 +0430

    feat : add ui pagination from mui

--- ecommerce/src/Pages/Home.js
+++ ecommerce/src/Pages/Home.js
@@ -1,15 +1,31 @@
 import { Box, Pagination, Stack } from '@mui/material';
 import CardProduct from 'components/CardProduct';
+import PaginationPage from 'components/PaginationPage';
 import useFetch from 'hooks/useFetch';
 import CustomerLayout from 'layout/CustomerLayout';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
 
+    //data from products database
     const {data , loading , error} = useFetch('products');
+
+    //pagination data
+    let [page, setPage] = useState(1);
+    const perPage = 6;
+  
+    const count = Math.ceil(data.length / perPage);
+    const product = PaginationPage(data, perPage);
+  
+    const handleChange = (e, p) => {
+      setPage(p);
+        product.jump(p);
+    };
+
+
     return <Box sx={{display : 'flex' , flexDirection : 'column' , alignItems : 'center'}}>
         <Box sx={{display : 'flex' , flexDirection : {md : 'row' , sm : 'column'} , justifyContent : 'space-around' , alignItems : 'center' , flexWrap : 'wrap' , py : '30px'}}>
-        {data.map(item => {
+        {product.currentData().map(item => {
             return <CardProduct 
                 imageSrc={`http://localhost:3002/files/${item.image}`}
                 titleCard={item.name}
@@ -19,8 +35,14 @@ const Home = () => {
         }) 
         }
         </Box> 
-        <Stack padding='40px'>
-            <Pagination count={10} color="secondary" />
+        <Stack padding='30px'>
+            <Pagination 
+                size="large" 
+                count={count} 
+                color="secondary" 
+                onChange={handleChange}
+                page={page}
+            />
         </Stack>
     </Box>
 };
