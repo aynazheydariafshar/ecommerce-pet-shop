090490ea4957cc50776793e5cbc2c986b6c5aaf8
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-06-02 01:17:32 +0430

    feat : add radio for toggle between components

--- ecommerce/src/Pages/Orders.js
+++ ecommerce/src/Pages/Orders.js
@@ -9,47 +9,27 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import useFetch from "hooks/useFetch";
-import useCategory from "hooks/useCategory";
-import { Button, Checkbox, FormControlLabel, Pagination, Radio, RadioGroup, Stack } from "@mui/material";
+import { Button, Checkbox, FormControlLabel, IconButton, Pagination, Radio, RadioGroup, Stack } from "@mui/material";
 import PaginationPage from "components/PaginationPage";
 import { Box } from "@mui/system";
+import {BiDetail} from 'react-icons/bi';
+import DeleveryOrders from 'components/DeleveryOrders';
+import WaiteOrders from 'components/WaiteOrders';
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
-  
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    "&:last-child td, &:last-child th": {
-      border: 0,
-    },
-  }));
 
 const Orders = () => {
 
-      //data
-  const { data, loading, error } = useFetch("orders");
+  //value radio
+  const [value, setValue] = React.useState("delevery");
 
-  // //pagination data
-  let [page, setPage] = React.useState(1);
-  const perPage = 10;
-
-  const count = Math.ceil(data.length / perPage);
-  const product = PaginationPage(data, perPage);
-
-  const handleChange = (e, p) => {
-    setPage(p);
-    product.jump(p);
-  };
+  //change radio click
+  function handleClick(event) {
+    if (event.target.value === 'waite') {
+      setValue('waite');
+    } else {
+      setValue('delevery');
+    }
+  }
 
     return <>
     <RadioGroup
@@ -59,59 +39,18 @@ const Orders = () => {
       >
         <FormControlLabel
           value="delevery" 
-          control={<Radio color="secondary"/>}
+          control={<Radio onClick={handleClick} color="secondary"/>}
           label="سفارش های تحویل شده"
           sx={{display : 'inline' , padding :'30px'}}
         />
         <FormControlLabel
           value="waite" 
-          control={<Radio color="secondary"/>}
+          control={<Radio onClick={handleClick} color="secondary"/>}
           label="سفارش های درانتظار ارسال"
           sx={{ paddingX :'30px'}}
         />
       </RadioGroup>
-    <Box
-        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-      >
-        <TableContainer dir="rtl" component={Paper} sx={{ padding: "30px" }}>
-          <Table sx={{ minWidth: 700 }} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>#</StyledTableCell>
-                <StyledTableCell align="right">نام </StyledTableCell>
-                <StyledTableCell align="right">نام خانوادگی</StyledTableCell>
-                <StyledTableCell align="right">مجموع مبلغ</StyledTableCell>
-                <StyledTableCell align="right">جزییات سفارش</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-            {product.currentData()?.map((row) => (
-                <StyledTableRow key={row.name}>
-                  <StyledTableCell component="th" scope="row">
-                    {row.id}
-                  </StyledTableCell>
-                  <StyledTableCell align="right" component="th" scope="row">
-                    {row.customerDetail.firstName}
-                  </StyledTableCell>
-                  <StyledTableCell align="right" component="th" scope="row">
-                    {row.customerDetail.lastName}
-                  </StyledTableCell>
-                </StyledTableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-
-        <Stack padding="30px">
-          <Pagination
-            size="large"
-            count={count}
-            color="secondary"
-            onChange={handleChange}
-            page={page}
-          />
-        </Stack>
-      </Box>
+      {value === 'delevery' ? <DeleveryOrders /> : <WaiteOrders />}
     </>;
 };
 
