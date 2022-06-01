277af36c7d9419a49982cc499bebab8047350051
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-06-01 12:31:10 +0430

    feat : add data on context

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -3,8 +3,25 @@ import RoutePages from 'routes/RoutePages';
 import { useEffect, useState } from 'react';
 import { getToken, removeUserSession, setUserSession } from 'utils/Common';
 import axios from 'axios';
+import { DataContext } from 'Context/DataContext';
 
 function App() {
+
+  const [loading , setLoading] = useState(true);
+  const [error , setError] = useState(null);
+  const [data , setdata] = useState([]);
+  
+  //get data
+  const getdata = () => {
+      setLoading(true);
+      axios.get(`http://localhost:3002/products`)
+      .then(response => {
+          setdata(response.data)
+      })
+      .catch(err => setError("لطفا دوباره تلاش کنید"))
+      .finally(res =>setLoading(false))
+  }
+
   useEffect(()=>{
     const token = getToken();
     if(!token){
@@ -14,11 +31,17 @@ function App() {
       .then(response => {
         setUserSession(response.data.token , response.data.user);
       }).catch(er => removeUserSession())
-  }, [])
+    getdata();
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <RoutePages />
-    </div>
+    <DataContext.Provider value={{ data , getdata}}>
+      <div className="App">
+        <RoutePages />
+      </div>
+    </DataContext.Provider>
   );
   
 }
