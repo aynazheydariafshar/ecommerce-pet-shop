0f0757e560d922af0ac6ab5d56d3954ceb7f9c45
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-06-02 22:32:01 +0430

    feat : add components cart

--- ecommerce/src/Pages/Product.js
+++ ecommerce/src/Pages/Product.js
@@ -7,6 +7,7 @@ import CustomerLayout from 'layout/CustomerLayout';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import {MdOutlineAddCircleOutline} from 'react-icons/md'
+import Cart from 'components/Cart';
 const Product = () => {
 
     const dataParams = useParams();
@@ -27,25 +28,8 @@ const Product = () => {
    return <>
         {product?.map(item =>{
             return (
-            <>  
-            <Box sx={{display : 'flex', flexDirection : 'column' , justifyContent : 'space-around', alignItems : 'center',}}>
-                <Box
-                    sx={{
-                    overflow : 'hidden',    
-                    width: '85%',
-                    height: '70%',
-                    display : 'flex',
-                    flexDirection : {md : 'row' , sm : 'column'},
-                    justifyContent : 'space-around',
-                    alignItems : 'center',
-                    background : '#F8F9FD',
-                    boxShadow: 'rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px',                  
-                    borderRadius : 2,
-                    padding : '75px',
-                    marginY : '30px',
-                    marginLeft : {md:'100px' , sm : '50px'},
-                }}
-                >
+            <>
+                <Cart>
                     <Box sx={{textAlign : 'right'}}>
                         <Typography variant="h5" sx={{fontWeight : 'bold' , paddingY : '10px'}} component="div">
                             {item.name} 
@@ -80,17 +64,15 @@ const Product = () => {
                                 {item.detailes?.map(i => <li style={{marginRight : '30px'}}>{i}</li>)}
                             </ul>    
                         </Typography>}
-                        <Button color = "secondary"  variant="contained" sx={{float : 'left' , marginTop : '20px'}} endIcon={<MdOutlineAddCircleOutline />}>
+                        <Button color = "secondary"  variant="contained" sx={{float : 'left' , marginTop : '20px' }} endIcon={<MdOutlineAddCircleOutline />}>
                             افزودن به سبد خرید
                         </Button>
                     </Box>
                     <Box padding='10px'>
                         <img src={`http://localhost:3002/files/${item.image}`} width="300px" height='300px'/>
                     </Box>
-                </Box>
-
-            </Box>  
-                {item.description && <Typography variant="h6" component="div" sx={{padding : '35px' , marginX : '50px' , textAlign : 'right'}}>
+                </Cart>
+                {item.description && <Typography variant="h6" component="div" sx={{padding : '35px' , marginX : '50px' , textAlign : 'right' , marginBottom : '150px'}}>
                    : توضیحات 
                     <div>{item.description} </div>
                 </Typography>}
