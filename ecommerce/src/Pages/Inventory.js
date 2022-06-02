edbcc3db6bc92d9deb20b1be956edb510197c466
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-06-02 17:52:04 +0430

    clean : clean code

--- ecommerce/src/Pages/Inventory.js
+++ ecommerce/src/Pages/Inventory.js
@@ -8,8 +8,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import useFetch from "hooks/useFetch";
-import useCategory from "hooks/useCategory";
 import { Button, Pagination, Stack } from "@mui/material";
 import PaginationPage from "components/PaginationPage";
 import { Box } from "@mui/system";
