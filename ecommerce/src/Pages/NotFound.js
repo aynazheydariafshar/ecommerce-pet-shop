1a3c875f5a788bf761323e85012b58bfe91fdca4
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-06-08 18:08:27 +0430

    feat : design notfound page

--- ecommerce/src/Pages/NotFound.js
+++ ecommerce/src/Pages/NotFound.js
@@ -1,7 +1,15 @@
+import Cart from 'components/Cart';
 import React from 'react';
+import notFound from 'assets/images/404 Error with a cute animal-amico.png';
+import { Typography } from '@mui/material';
 
 const NotFound = () => {
-    return <div>NotFound</div>;
+    return <Cart width='60%' padding='20px'>
+        <img src={notFound} width='400px' height='400px' alt='NotFound'/>
+        <Typography variant='h5' fontWeight='bold'>
+            صفحه موردنظر پیدا نشد
+        </Typography>
+    </Cart>;
 }
 
 
